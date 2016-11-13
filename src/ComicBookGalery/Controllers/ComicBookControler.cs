8736cbc0cf4145c4e4a43beab9068d8a232f1e4b
masamunevntn <masamunevntn@gmail.com>
2016-11-14 02:09:23 +0700

    add Detail action method

---
+++ src/ComicBookGalery/Controllers/ComicBookControler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGalery.Controllers
+{
+    public class ComicBookControler : Controller
+    {
+        public string Detail()
+        {
+            return "hello from comic book controler";
+        }
+    }
+}
